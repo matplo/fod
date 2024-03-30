570b49cdd8948671443674e4f6a944dca4e6e877
matplo <ploskon@gmail.com>
2024-03-29 23:17:42 -0700

    add test user

--- make_db_prod.sh
+++ make_db_prod.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+user_manip() {
+		docker-compose exec web python user_manip.py $@
+}
+user_manip add --username test --password test --email test@test.com
+
 docker-compose exec web python manage.py create_db
 docker-compose exec web python manage.py seed_db
 
@@ -9,3 +14,5 @@ docker-compose exec db psql --username=hello_flask --dbname=hello_flask_prod -c 
 docker-compose exec db psql --username=hello_flask --dbname=hello_flask_prod -c "SELECT * FROM users;"
 
 docker volume inspect flask-on-docker_postgres_data
+
+user_manip list
