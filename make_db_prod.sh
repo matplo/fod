67834f12c71993078e3b6df0a82ce46a2033e86d
matplo <ploskon@gmail.com>
2024-04-03 10:40:40 -0700

    assume unchanged users.yaml; call docker-compose with -f ...

--- make_db_prod.sh
+++ make_db_prod.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 user_manip() {
-		docker-compose exec web python user_manip.py $@
+	docker-compose -f docker-compose.prod.yml exec web python user_manip.py $@
 }
 user_manip add --username test --password test --email test@test.com
 
-docker-compose exec web python manage.py create_db
-docker-compose exec web python manage.py seed_db
+docker-compose -f docker-compose.prod.yml exec web python manage.py create_db
+docker-compose -f docker-compose.prod.yml exec web python manage.py seed_db
 
 # this is to check if the db exists
-docker-compose exec db psql --username=hello_flask --dbname=hello_flask_prod -c "\l"
-docker-compose exec db psql --username=hello_flask --dbname=hello_flask_prod -c "\dt"
-docker-compose exec db psql --username=hello_flask --dbname=hello_flask_prod -c "SELECT * FROM users;"
+docker-compose -f docker-compose.prod.yml exec db psql --username=hello_flask --dbname=hello_flask_prod -c "\l"
+docker-compose -f docker-compose.prod.yml exec db psql --username=hello_flask --dbname=hello_flask_prod -c "\dt"
+docker-compose -f docker-compose.prod.yml exec db psql --username=hello_flask --dbname=hello_flask_prod -c "SELECT * FROM users;"
 
 # docker volume inspect flask-on-docker_postgres_data
 
--- user_manip.sh
+++ user_manip.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker-compose exec web python user_manip.py $@
+OS=$(uname)
+DOCKER_VERSION=$(docker -v | cut -d ' ' -f3 | cut -d ',' -f1)
+
+docker-compose -f docker-compose.prod.yml exec web python user_manip.py $@
